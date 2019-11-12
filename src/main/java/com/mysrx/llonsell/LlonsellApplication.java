01c55b7188f601d13ece152a8d2e4c4391e65415
qiang8696 <460782707@qq.com>
2019-11-12 17:36:29 +0800

    提交项目

---
+++ src/main/java/com/mysrx/llonsell/LlonsellApplication.java
@@ -0,0 +1,13 @@
+package com.mysrx.llonsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlonsellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlonsellApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mysrx/llonsell/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.mysrx.llonsell.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+
+public class HelloController {
+    @RequestMapping("/myhello")
+    @ResponseBody
+    public String sayhello()
+    {
+        System.out.println("hello");
+        return "hellllll";
+    }
+}
